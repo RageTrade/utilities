fb5ac07c02fe8cb3b0bb88994aa04fb6fcee26bb
protodev-rage <protodev@rage.trade>
2022-03-19 17:00:39 +0530

    new: types

---
+++ types/index.ts
@@ -0,0 +1,35 @@
+import { BigNumber, BigNumberish } from "ethers"
+
+export enum Side {
+  BUY,
+  SELL,
+}
+
+export interface Position {
+  tickUpper: number
+  tickLower: number
+  vTokenAmount: BigNumber
+}
+
+export interface NetRagePosition {
+  netSide: Side
+  netTokenPosition: Position
+  availableMargin: BigNumberish
+  accountMarketValue: BigNumberish
+  lastUpdated: Date | BigNumberish
+  lastTradedFundingRate: BigNumberish
+}
+
+export interface NetFtxPosition {
+  netSide: Side
+  availableMargin: BigNumberish
+  netTokenPosition: BigNumberish
+  accountMarketValue: BigNumberish
+  lastUpdated: Date | BigNumberish
+  lastTradedFundingRate: BigNumberish
+}
+
+export interface LiquidityInfo {
+  tick: number,
+  liquidity: BigNumberish
+}
