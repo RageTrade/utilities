579dc84f1eb7a0f116e8cc76bb851a5ef9b0ce8f
protodev-rage <protodev@rage.trade>
2022-10-10 11:44:01 +0530

    fix: batching manager

--- batching-manager/index.ts
+++ batching-manager/index.ts
@@ -5,8 +5,13 @@ import { ethers } from 'ethers'
 import { log } from '../discord-logger'
 import { NETWORK_INF0 } from '../config-env'
 
-import { getGmxVaultContracts, GMXBatchingManager } from '@ragetrade/sdk'
+import {
+  getGmxVaultContracts,
+  GlpStakingManager,
+  GMXBatchingManager,
+} from '@ragetrade/sdk'
 
+let glpStakingManager: GlpStakingManager
 let gmxBatchingManager: GMXBatchingManager
 
 const provider = new ethers.providers.StaticJsonRpcProvider(
@@ -17,12 +22,23 @@ const signer = new ethers.Wallet(NETWORK_INF0.PK_BATCHING_MANAGER, provider)
 
 const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms))
 
-const executeBatch = async (bm: GMXBatchingManager) => {
+const executeBatch = async (bm: GMXBatchingManager, sm: GlpStakingManager) => {
+  try {
+    const tx1 = await sm.harvestFees()
+    await tx1.wait()
+
+    log(
+      `fees harvested, ${NETWORK_INF0.BLOCK_EXPLORER_URL}tx/${tx1.hash}`,
+      'BATCHING_MANAGER'
+    )
+  } catch (e: any) {
+    log(`failed fees harvesting, ${e.body}, ${e.message}`, 'BATCHING_MANAGER')
+  }
+
   try {
     const tx1 = await bm.pauseDeposit()
     await tx1.wait()
-    await sleep(15 * 1000)
-
+    await sleep(15 * 60 * 1000 + 30 * 1000)
     log(
       `pause + 15 min wait success, batch will execute now, ${NETWORK_INF0.BLOCK_EXPLORER_URL}tx/${tx1.hash}`,
       'BATCHING_MANAGER'
@@ -32,10 +48,22 @@ const executeBatch = async (bm: GMXBatchingManager) => {
   }
 
   try {
-    const tx2 = await bm.executeBatchDeposit()
-    await tx2.wait()
+    const tx1 = await bm.unpauseDeposit()
+    await tx1.wait()
+
     log(
-      `batch exeucuted, ${NETWORK_INF0.BLOCK_EXPLORER_URL}tx/${tx2.hash}`,
+      `deposits unpaused, ${NETWORK_INF0.BLOCK_EXPLORER_URL}tx/${tx1.hash}`,
+      'BATCHING_MANAGER'
+    )
+  } catch (e: any) {
+    log(`failed unpausing, ${e.body}, ${e.message}`, 'BATCHING_MANAGER')
+  }
+
+  try {
+    const tx1 = await bm.executeBatchDeposit()
+    await tx1.wait()
+    log(
+      `batch exeucuted, ${NETWORK_INF0.BLOCK_EXPLORER_URL}tx/${tx1.hash}`,
       'BATCHING_MANAGER'
     )
   } catch (e: any) {
@@ -47,9 +75,11 @@ const executeBatch = async (bm: GMXBatchingManager) => {
 }
 
 ;(async () => {
-  ;({ gmxBatchingManager } = await getGmxVaultContracts(signer))
+  ;({ gmxBatchingManager, glpStakingManager } = await getGmxVaultContracts(
+    signer
+  ))
   cron.schedule('0 */12 * * *', () => {
-    executeBatch(gmxBatchingManager)
+    executeBatch(gmxBatchingManager, glpStakingManager)
       .then(() => console.log('RUN COMPLETE!'))
       .catch((error) => {
         console.error(error)
